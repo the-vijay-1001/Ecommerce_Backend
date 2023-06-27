d651a22a274254c1ff5724ec6e5dcce2f1509cfc
vijay vishwakarma <vijay.vishwakarma@codiant.com>
2023-06-27 07:04:05 -0700

    product listing by its vendor ID

--- src/repositories/product-repositories.js
+++ src/repositories/product-repositories.js
@@ -1,31 +1,49 @@
 import models from "../models";
-const { product ,productImage,media } = models;
+const { product, productImage, media } = models;
 export default {
-    async uploadProduct(request) {
-        const bodyData = request.body;
-        console.log(bodyData);
-        const productData = await product.create(bodyData);
-        return productData;
-    },
-    async productList(){
-        const products=await product.findAll({
-            include: [
-                {
-                  model: productImage,
-                  include: [
-                    {
-                      model: media, 
-                    },
-                  ],
-                },
-              ],
-        });
-        return products;
-    },
-    async uploadProductImage(request) {
-        const {imageId } = request;
-        console.log(imageId);
-        const productImages = await productImage.create(request);
-        return productImages;
-    },
+  async uploadProduct(request) {
+    const bodyData = request.body;
+    const productData = await product.create(bodyData);
+    return productData;
+  },
+  async productList() {
+    const products = await product.findAll({
+      include: [
+        {
+          model: productImage,
+          include: [
+            {
+              model: media,
+            },
+          ],
+        },
+      ],
+    });
+    return products;
+  },
+
+  async productListById(request) {
+    const { vendorId } = request.body;
+    const productList = await product.findAll({
+      where: { vendorId: vendorId },
+      include: [
+        {
+          model: productImage,
+          include: [
+            {
+              model: media,
+            },
+          ],
+        },
+      ],
+    });
+    return productList;
+  },
+
+  async uploadProductImage(request) {
+    const { imageId } = request;
+    console.log(imageId);
+    const productImages = await productImage.create(request);
+    return productImages;
+  },
 }
